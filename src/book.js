7ba94570feb86dc4bcf9ca6bbbe1009546c1f01c
John Hennerich <john.hennerich@gmail.com>
2022-11-02 15:18:24 -0600

    Develop functions

--- src/book.js
+++ src/book.js
@@ -1,12 +1,29 @@
-function createTitle() {
+function createTitle(title) {
+  return "The " + title
   
+};
+
+function buildMainCharacter(name, age, pronouns) {
+  var character = { 
+    name: name,
+    age: age,
+    pronouns: pronouns
+  };
+  return character;
+
+};
+
+function saveReview(review, reviews) {
+  if (!reviews.includes(review) ) { 
+    return reviews.push(review) 
+  } 
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  //writeBook,
+  //editBook
 }
