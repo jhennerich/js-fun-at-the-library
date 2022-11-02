531c9438b1ae60dd827d0063523309cb6936bac1
John Hennerich <john.hennerich@gmail.com>
2022-11-02 15:39:47 -0600

    All tests passing

--- src/book.js
+++ src/book.js
@@ -19,11 +19,32 @@ function saveReview(review, reviews) {
   } 
 }
 
+function calculatePageCount(title) {
+  let page_count = 20 * title.length;
+  return page_count;
+}
+
+function writeBook(title, mainCharacter, genre) {
+
+  const book = {
+    title: title,
+    mainCharacter: mainCharacter,
+    pageCount: calculatePageCount(title),
+    genre: genre
+  }
+  return book;
+}
+
+function editBook(book) {
+  book.pageCount = book.pageCount * .75;
+  return book;
+}
+
 module.exports = {
   createTitle,
   buildMainCharacter,
   saveReview,
   calculatePageCount,
-  //writeBook,
-  //editBook
+  writeBook,
+  editBook
 }
